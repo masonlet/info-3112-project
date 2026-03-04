6fc6969588857bf9d8459930069d3ef9c7f1e3b8
Mason L'Etoile <contact@masonletoile.ca>
2026-03-04 13:56:45 -0500

    chore: remove unneeded template files

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Info 3112 Project",
+  description: "Fanshawe group project",
 };
 
 const geistSans = Geist({
