b598b1d2bbb0666897d417521f5a1850a47c1c4e
Masonlet <masonletoile@hotmail.com>
2026-03-19 21:00:26 -0400

    chore: add Vitest coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,22 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from "vitest/config";
+import path from "path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    include: ['tests/**/*.{test,spec}.{ts,tsx}']
-  }
+    include: ['tests/**/*.{test,spec}.{ts,tsx}'],
+    coverage: {
+      provider: "v8",
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, "./"),
+    },
+  },
 })
