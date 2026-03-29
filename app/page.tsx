3420e7572884e85c433ff897a6e130f210172f9d
Masonlet <masonletoile@hotmail.com>
2026-03-29 16:54:00 -0400

    fix: add muted background to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center">
+    <div className="flex flex-col flex-1 items-center justify-center bg-muted/30">
       <h1 className="text-2xl font-semibold">Info-3112 Group Project</h1>
     </div>
   );
