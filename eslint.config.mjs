c379547f5bcb8b53c69e474a80abfaa03a36cef5
Ahmed Ibrahim <ahmed.mardi09@gmail.com>
2026-04-02 20:59:47 -0400

    Add eslint rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,6 +12,12 @@ const compat = new FlatCompat({
 const eslintConfig = [
   { ignores: ["node_modules/**", ".next/**"] },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off"
+    }
+  }
 ];
 
 export default eslintConfig;
