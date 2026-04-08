3415988da16fb77e82bb416f2fd263b0e7c88ebb
Masonlet <masonletoile@hotmail.com>
2026-04-08 11:52:41 -0400

    fix: remove no-unused-vars

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,12 +12,6 @@ const compat = new FlatCompat({
 const eslintConfig = [
   { ignores: ["node_modules/**", ".next/**"] },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-
-  {
-    rules: {
-      "@typescript-eslint/no-unused-vars": "off"
-    }
-  }
 ];
 
 export default eslintConfig;
